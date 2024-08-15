fe52d964333b1c5f9252fbd624750c47899fd34c
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2024-08-15 21:48:04 +0200

    Function declarations, declref sema tests

--- tests/sema_tests.cpp
+++ tests/sema_tests.cpp
@@ -51,20 +51,79 @@ fn void main() {
     a();
 }
 )");
-    REQUIRE(output_buffer.str().size() == 0);
+    REQUIRE(output_buffer.str().empty());
     REQUIRE(error_stream.str() ==
             "sema_test:3:5 error: symbol 'a' undefined.\n");
   }
+  SECTION("Incorrect parameter types") {
+    TEST_SETUP(R"(
+fn void foo(){}
+
+fn void bar(i32 a, i32 b){}
+
+fn void main() {
+  foo(1);
+  bar(foo(), foo());
+  bar(1.0, foo());
+  bar();
+  bar(1, 2, 3);
+  bar(1, 2);
+  foo();
+}
+)");
+    REQUIRE(output_buffer.str().empty());
+    REQUIRE(error_stream.str() ==
+            R"(sema_test:7:6 error: argument count mismatch.
+sema_test:8:10 error: unexpected type 'void', expected 'i32'.
+sema_test:9:7 error: unexpected type 'f32', expected 'i32'.
+sema_test:10:6 error: argument count mismatch.
+sema_test:11:6 error: argument count mismatch.
+sema_test:12:7 error: unexpected type 'u8', expected 'i32'.
+)");
+  }
 }
 
-/*
-fn void baz(){}
+TEST_CASE("Declref", "[sema]") {
+  SECTION("Using function as variable") {
+    TEST_SETUP(R"(
+fn void foo(){}
 
-fn void main(){
-    baz();
-    bar();
-    a();
+fn void main() {
+  foo;
+  y;
+  foo();
+}
+)");
+    REQUIRE(output_buffer.str().empty());
+    REQUIRE(error_stream.str() ==
+            R"(sema_test:5:3 error: expected to call function 'foo',
+sema_test:6:3 error: symbol 'y' undefined.
+)");
+  }
 }
 
-fn void bar(){}
-*/
+TEST_CASE("Function parameters", "[sema]") {
+  SECTION("Unknown paramatere type") {
+    TEST_SETUP(R"(
+fn void foo(u32 a, CustomType b) {}
+)");
+    REQUIRE(
+        error_stream.str() ==
+        "sema_test:2:20 error: parameter 'b' has invalid 'CustomType' type\n");
+    REQUIRE(output_buffer.str().empty());
+  }
+  SECTION("Invalid parameter 'void'") {
+    TEST_SETUP(R"(
+fn void foo(void a, u32 b){}
+)");
+    REQUIRE(output_buffer.str().empty());
+    REQUIRE(error_stream.str() ==
+            "sema_test:2:13 error: invalid paramater type 'void'.\n");
+  }
+  SECTION("Parameter redeclaration") {
+    TEST_SETUP(R"(
+fn void foo(i32 x, f32 x){}
+)");
+    REQUIRE(output_buffer.str().empty());
+  }
+}
