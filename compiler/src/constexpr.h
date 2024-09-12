609b9f0b00b5461a8815fa0f9e3a57e45f1cc186
Dominik <dominik.kurasbediani@gmail.com>
2024-09-12 15:20:50 +0200

    Variable decl constexpr implementation, tests

--- compiler/src/constexpr.h
+++ compiler/src/constexpr.h
@@ -14,5 +14,7 @@ private:
   eval_binary_op(const ResolvedBinaryOperator &binop);
   std::optional<ConstexprResult>
   eval_unary_op(const ResolvedUnaryOperator &unop);
+  std::optional<ConstexprResult>
+  eval_decl_ref_expr(const ResolvedDeclRefExpr& ref);
 };
 } // namespace saplang
