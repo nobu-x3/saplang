9ff13817ccd4fe1c159a3c31b871cd70daf91c34
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2024-12-17 21:20:26 +0100

    Driver refactor, modules tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-cmake -B build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=On && make -C build -j 32 && build/bin/saplang_tests
+rm -rf build/bin/module_tests
+cmake -B build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=On && make -C build -j 32
+cp -r tests/module_tests build/bin/module_tests
+build/bin/saplang_tests
