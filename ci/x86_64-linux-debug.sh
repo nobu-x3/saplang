2021a715ba328aad8dcfd90920dfd3044b402f36
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2024-12-19 20:06:25 +0100

    CI fix after module test introduction

--- ci/x86_64-linux-debug.sh
+++ ci/x86_64-linux-debug.sh
@@ -14,7 +14,11 @@ git fetch --tags
 
 git clean -fd
 
+rm -rf build/tests/build
 cmake -B build -G "$GENERATOR" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBUILD_TESTS=On
 make -C build
+mkdir build/tests/build
+mkdir build/tests/build/bin
+cp -r tests/module_tests build/tests/build/bin/module_tests
 cd build
 ctest -C $BUILD_TYPE --output-on-failure
