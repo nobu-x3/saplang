850cffe1c8ac54abebff1a702bc8873cdca8b8a2
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2025-02-18 21:06:05 +0100

    Workflows wip

--- ci/arm_linux_build.sh
+++ ci/arm_linux_build.sh
@@ -9,25 +9,14 @@ git fetch --tags
 
 git clean -fd
 
+mkdir build
+mkdir bulid/nightly
+mkdir build/nightly/ARM-linux
+
 ARCH="$(uname -m)"
 TARGET="$ARCH-linux-musl"
 MCPU="baseline"
 GENERATOR="Unix Makefiles"
-
-# Build x86
-# # Debug
-BUILD_TYPE=Debug
-cmake -DCMAKE_TOOLCHAIN_FILE=toolchain-x86_64.cmake -B build -G "$GENERATOR" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBUILD_TESTS=Off
-make -C build -j 3
-mkdir build/nightly/x86_64-linux/Debug
-mv build/bin/compiler build/nightly/x86_64-linux/Debug/saplangc
-
-# # Release
-BUILD_TYPE=Release
-cmake -DCMAKE_TOOLCHAIN_FILE=toolchain-x86_64.cmake -B build -G "$GENERATOR" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBUILD_TESTS=Off
-make -C build -j 3
-mkdir build/nightly/x86_64-linux/Release
-mv build/bin/compiler build/nightly/x86_64-linux/Release/saplangc
 
 # Build ARM
 # # Debug
