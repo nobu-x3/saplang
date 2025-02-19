94505118f558189478b83a63a79601c5a36c00ac
Dominik <dominik.kurasbediani@gmail.com>
2025-02-19 12:49:25 +0100

    Added build scripts for linux

---
+++ ci/x86_64-linux-build.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -x
+set -e
+
+rm -rf build
+
+# Get latest
+git fetch --unshallow || true
+git fetch --tags
+
+git clean -fd
+
+mkdir build
+mkdir build/nightly
+mkdir build/nightly/x86_64-linux
+
+ARCH="$(uname -m)"
+TARGET="$ARCH-linux-musl"
+MCPU="baseline"
+GENERATOR="Unix Makefiles"
+
+# Build x86
+# # Debug
+BUILD_TYPE=Debug
+cmake -B build -G "$GENERATOR" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBUILD_TESTS=Off
+make -C build
+mkdir build/nightly/x86_64-linux/Debug
+mv build/bin/compiler build/nightly/x86_64-linux/Debug/saplangc
+
+# # Release
+BUILD_TYPE=Release
+cmake -B build -G "$GENERATOR" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBUILD_TESTS=Off
+make -C build
+mkdir build/nightly/x86_64-linux/Release
+mv build/bin/compiler build/nightly/x86_64-linux/Release/saplangc
