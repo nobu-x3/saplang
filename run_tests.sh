6cc78a8849e4decb009011fe5589157c1aa8c230
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2025-03-01 20:21:36 +0100

    Set up cmake to compile the reworked compiler

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 rm -rf build/bin/module_tests
 cmake -B build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=On && make -C build -j 32
-cp -r tests/module_tests build/bin/module_tests
-build/bin/saplang_tests
+build/bin/saplangc
+# cp -r tests/module_tests build/bin/module_tests
+# build/bin/saplang_tests
