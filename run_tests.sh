ca5c79081c8a38509a20a01a2119b79256f39ef0
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2025-03-02 22:21:32 +0100

    Adds Unity testing framework and sets it up

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 rm -rf build/bin/module_tests
+
+ruby build/_deps/unity-src/auto/generate_test_runner.rb rework_tests/parser_tests.c rework_tests/.runners/parser_tests_runners.c
+
 cmake -B build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=On && make -C build -j 32
-build/bin/saplangc
+build/bin/saplangc_tests
+
+# build/bin/saplangc
 # cp -r tests/module_tests build/bin/module_tests
 # build/bin/saplang_tests
