b4485560992f253cbcd2bc3de56192fe3fa3fea7
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2025-03-27 20:59:58 +0100

    Linux CD changes to testing

--- ci/arm64-linux-debug-tests.sh
+++ ci/arm64-linux-debug-tests.sh
@@ -17,8 +17,9 @@ git clean -fd
 rm -rf build/tests/build
 cmake -B build -G "$GENERATOR" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBUILD_TESTS=On
 make -C build -j 2
-mkdir build/tests/build
-mkdir build/tests/build/bin
-cp -r tests/module_tests build/tests/build/bin/module_tests
-# cd build
-ctest -C $BUILD_TYPE --output-on-failure --progress --test-dir build
+# mkdir build/tests/build
+# mkdir build/tests/build/bin
+# cp -r tests/module_tests build/tests/build/bin/module_tests
+# # cd build
+# ctest -C $BUILD_TYPE --output-on-failure --progress --test-dir build
+build/bin/saplangc_tests
--- ci/x86_64-linux-debug-tests.sh
+++ ci/x86_64-linux-debug-tests.sh
@@ -9,16 +9,18 @@ MCPU="baseline"
 BUILD_TYPE=Debug
 GENERATOR="Unix Makefiles"
 
-git fetch --unshallow || true
-git fetch --tags
+# git fetch --unshallow || true
+# git fetch --tags
 
-git clean -fd
+# git clean -fd
 
 rm -rf build/tests/build
 cmake -B build -G "$GENERATOR" -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBUILD_TESTS=On
 make -C build
-mkdir build/tests/build
-mkdir build/tests/build/bin
-cp -r tests/module_tests build/tests/build/bin/module_tests
-cd build
-ctest -C $BUILD_TYPE --output-on-failure
+# mkdir build/tests/build
+# mkdir build/tests/build/bin
+# cp -r tests/module_tests build/tests/build/bin/module_tests
+# cd build
+# ctest -C $BUILD_TYPE --output-on-failure
+#
+build/bin/saplangc_tests
