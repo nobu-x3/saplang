bc4133bc8c27688b24309f7a40fbd79dafa8ce2e
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2026-07-31 22:33:05 +0200

    tests: drop the -Wall cmake flag rejected by cmake 4.4

--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p build/bin
 cp -r tests/module_tests build/bin/module_tests
 
-cmake -B build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=On -DLLVM_DIR=/usr/lib/llvm19/lib/cmake/llvm -Wall && make -C build -j 32
+cmake -B build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=On -DLLVM_DIR=/usr/lib/llvm19/lib/cmake/llvm && make -C build -j 32
 ctest --test-dir build --output-on-failure
 #(cd build/bin && rm -rf .tmp &&./saplangc_tests)
 
